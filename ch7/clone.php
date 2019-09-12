4a03df3629d183efff83ec11fb139f2834dce26b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-12 13:18:42 +0200

    create function __clone to execute when i use clone keyword

--- ch7/clone.php
+++ ch7/clone.php
@@ -4,22 +4,28 @@
 		private $employeeid;
 		private $tiecolor;
 		//define setter and getter
-		function setEmployeeID($employeeid)
+		
+		public function setEmployeeID($employeeid)
 		{
 			$this->employeeid = $employeeid;
 		}
-		function getEmployeeID()
+		public function getEmployeeID()
 		{
 			return $this->employeeid;
 		}
-		function setTiecolor($tiecolor)
+		public function setTiecolor($tiecolor)
 		{
 			$this->tiecolor = $tiecolor;
 		}
-		function getTiecolor()
+		public function getTiecolor()
 		{
 			return $this->tiecolor;
 		}
+		public function __clone()
+		{
+			$this->setTiecolor("Blue");
+		}
+		
 	}
 
 ?>
@@ -32,13 +38,20 @@
 	//set tiecolor
 	$employee1->setTiecolor("red");
 
-	echo "{$employee1->getEmployeeID()}employee1<br>";
-	echo "{$employee1->getTiecolor()}employee1<br>";
+	// echo "{$employee1->getEmployeeID()}employee1<br>";
+	// echo "{$employee1->getTiecolor()}employee1<br>";
+	$employee2 = clone $employee1;
+	$employee2->setEmployeeID("145236");
+	printf("Employee 1 ID %d <br>",$employee1->getEmployeeID());
+	printf("Employee 2 ID %d <br>",$employee2->getEmployeeID());
+	printf("Employee 1 tie %s <br>",$employee1->getTiecolor());
+	printf("Employee 2 tie %s <br>",$employee2->getTiecolor())
 
 	//clone object
-	$employee2 = clone $employee1;
-	$employee2->setEmployeeID("45745");
-	echo "{$employee2->getEmployeeID()}employee2<br>";
-	echo "{$employee2->getTiecolor()}employee2<br>";
+	// $employee2 = clone $employee1;
+	// $employee2->setEmployeeID("45745");
+	// echo "{$employee2->getEmployeeID()}employee2<br>";
+	// echo "{$employee2->getTiecolor()}employee2<br>";
 	// print_r(is_a($employee2, Employee));
+
 	?>
