8fba2d8cfbed2b2394aad2cc934da3b957888668
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-12 12:49:20 +0200

    create instances from class Employee and test clone keyword

--- ch7/clone.php
+++ ch7/clone.php
@@ -23,3 +23,22 @@
 	}
 
 ?>
+<?php
+	//create new employee instance
+	$employee1 = new Employee();
+	//set employee ID
+	$employee1->setEmployeeID("123456");
+
+	//set tiecolor
+	$employee1->setTiecolor("red");
+
+	echo "{$employee1->getEmployeeID()}employee1<br>";
+	echo "{$employee1->getTiecolor()}employee1<br>";
+
+	//clone object
+	$employee2 = clone $employee1;
+	$employee2->setEmployeeID("45745");
+	echo "{$employee2->getEmployeeID()}employee2<br>";
+	echo "{$employee2->getTiecolor()}employee2<br>";
+	// print_r(is_a($employee2, Employee));
+	?>
