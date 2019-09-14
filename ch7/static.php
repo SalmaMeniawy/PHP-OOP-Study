d895ced0d98f9b93c125baa4ea4c2de39549bfd4
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-14 14:57:24 +0200

    create test for static keyword for method and properties

--- ch7/static.php
+++ ch7/static.php
@@ -6,12 +6,21 @@ class Employee
 	{
 		echo "watching ".self::$favSport;
 	}
+	public static function type()
+	{
+		echo "Hello from type parent function";
+	}
 }
 class Executive extends Employee
 {
 	public static $favSport = "polp";
-
+	public static function type()
+	{
+		echo "hello from child class";
+	}
+	 
 }
 echo Executive::watchTv();
+echo Executive::type();
 echo Executive::$favSport;
 ?>
