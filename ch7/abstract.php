95222bfbd26a4159d845e4a6ab7480f05822aa6c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-16 14:56:03 +0200

    create instance from Library and DataCleaner which poth of them contain clean class

--- ch7/abstract.php
+++ ch7/abstract.php
@@ -3,7 +3,19 @@
 	require "Library.inc.php";
 	use Library;
 	use DataCleaner;
-	
+	// Instantiate the Library's Clean class
+	$filter = new Library\Clean();
+
+	// Instantiate the DataCleaner's Clean class
+	$profanity = new DataCleaner\Clean();
+
+	//create a book title
+	$title = "the idiotic sun also rises";
+
+	// Output the title before filtering occurs
+	printf("Title before filters: %s <br />", $title);
+
+
 	abstract class Media
 	{
 		private $title ;
