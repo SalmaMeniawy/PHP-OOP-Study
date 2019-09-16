4b5305ad7cd5f8a1b4512e885a106d5eeb5ab3d1
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-16 14:34:12 +0200

    add require for our libraries DataCleaner and Library

--- ch7/abstract.php
+++ ch7/abstract.php
@@ -1,4 +1,7 @@
 <?php
+	require "DataCleaner.inc.php";
+	require "Library.inc.php";
+
 	abstract class Media
 	{
 		private $title ;
@@ -14,23 +17,24 @@
 	{
 		function setDescription($description)
 		{
-
+			echo "<br>hello from setDescription Newspaper <br>";
 		}
 		function setSubscribers($subscribers)
 		{
-			
+				
 		}
 	}
 	class CD extends Media
 	{
 		function setDescription($description)
 		{
-
+			echo "<br>Hello from CD<br>";
 		}
 		function setCopiesSolid($subscribers)
 		{
-
+			echo "<br>hello from setCopiesSolid CD<br>";
 		}
 	}
+
 	// $emp = new Media();
 ?>
