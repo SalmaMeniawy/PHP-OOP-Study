a5344ec68e9e56bfa56d6ed854091cf5f679f598
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-16 14:33:35 +0200

    create function in class clean  removeProfanity

--- ch7/DataCleaner.inc.php
+++ ch7/DataCleaner.inc.php
@@ -1,6 +1,14 @@
 <?php
 	class Clean
 	{
-		
+		function removeProfanity($text)
+		{
+			$badwords = array("idiotic"=>"shortsighted",
+							  "moronic"=>"unreasonable",
+							  "insane"=>"illogical");
+			// Replace bad words
+			return strtr($text,$badwords); 	
+			
+		}
 	}
 ?>
