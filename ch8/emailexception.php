3cb262adc1c4b263d79436670cea24b4c3e4c277
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-17 16:22:56 +0200

    override the construct of the base class and take message and email

--- ch8/emailexception.php
+++ ch8/emailexception.php
@@ -1,6 +1,14 @@
 <?php
+
+	/* The InvalidEmailException class notifies the
+	administrator if an e-mail is deemed invalid. */
+
 	class InvalidEmailException extends Exception
 	{
-		
+		function __construct($message ,$email)
+		{
+			$this->email = $email;
+			$this->message = $message;
+		}
 	}
 ?>
