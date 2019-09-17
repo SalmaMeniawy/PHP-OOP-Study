2832b169810a0040d30469835436a1db1598c26b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-17 19:24:37 +0200

    fix the syntax

--- ch13/index.php
+++ ch13/index.php
@@ -4,7 +4,7 @@
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		print_f("Hi -- %s",$name);
-		print_f("<br>Address is %s  will soon be a spam-magnet! ",$email)
+		print_f("<br>Address is %s  will soon be a spam-magnet! ",$email);
 	}
 
 ?>
