dd88d3884aacd7070bae8ffbddc991ce81471400
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-19 14:47:56 +0200

    try htmlentities function

--- ch13/subscribe.php
+++ ch13/subscribe.php
@@ -1,6 +1,10 @@
-<!-- $userInput = "I am going to hax0r your site, hahaha!
+<?php
+$userInput = "I am going to hax0r your site, hahaha!
 	<script type='text/javascript'>
 	window.location = 'http://www.example.com/'
 	</script>'";
 
-$userInputEntities = htmlentities($userInput); -->
+$userInputEntities = htmlentities($userInput);
+echo "$userInputEntities<br>";
+
+?>
