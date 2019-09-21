730cab7eb29867c40d86ebba720b77f6da8ae7d6
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-21 18:04:43 +0200

    try example

--- ch13/subscribe.php
+++ ch13/subscribe.php
@@ -16,6 +16,31 @@ $input = strip_tags($input,"<i> </i>");
 $email = "salma@@gmail.com";
 if(!filter_var($email,FILTER_VALIDATE_EMAIL))
 {
-	echo " Invalid Email";
+	// echo " Invalid Email";
 }
+
+?>
+<select name="languages[]" multiple="multiple">
+	<option value="csharp">C#</option>
+	<option value="javascript">JavaScript</option>
+	<option value="perl">Perl</option>
+	<option value="php" selected>PHP</option>
+</select>
+<p>
+	<input type="submit" name="submit" value="submit">
+</p>
+<?php
+	if(isset($_POST['submit']))
+	{
+		echo "You like the following languages:<br>";
+		if(is_array($_POST['languages']))
+		{
+			foreach($_POST['languages'] AS $language) {
+				$language = htmlentities($language);
+				echo "$language<br>";
+				}
+
+		}
+	}
+
 ?>
