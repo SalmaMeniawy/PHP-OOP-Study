f6e8ec75709b9633adcbbd47c5b250b3dbe0c88e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-23 22:22:46 +0200

    try super variable in authentication

--- ch14/index.php
+++ ch14/index.php
@@ -1,7 +1,17 @@
 <?php
+$_SERVER['PHP_AUTH_USER']="salma";
+$_SERVER['PHP_AUTH_PW']="1234";
+// header('WWW-Authenticate: Basic Realm="Book Projects"');
+// header("HTTP/1.1 401 Unauthorized");
+if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']))
+{
+	header('WWW-Authenticate: Basic Realm="Authentication"');
+	header("HTTP/1.1 200 OK");
 
-header('WWW-Authenticate: Basic Realm="Book Projects"');
-header("HTTP/1.1 401 Unauthorized");
-
-
+}
+//if username and password are available 
+else{
+	echo "Your supplied username :{$_SERVER['PHP_AUTH_USER']}";
+	echo "Your Password :{$_SERVER['PHP_AUTH_PW']}";
+} 
 ?>
