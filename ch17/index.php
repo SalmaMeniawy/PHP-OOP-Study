82e0861bc831458e839c7dbe03c6ed963b2792a0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-26 21:46:24 +0200

    try the function Setcookie in php

---
+++ ch17/index.php
@@ -0,0 +1,8 @@
+<?php
+    $name = "salma";
+    $age = 23;
+   //send cookie by function setcookie and take name and value
+   setcookie('cookieName','this is the cookie value');
+   setcookie($name ,$age);
+
+?>
