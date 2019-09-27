4a6a0f36121919fc81de41bc9a9e3d113155b96d
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-27 16:12:05 +0200

    try method session decode in php

--- ch17/session/encode_session.php
+++ ch17/session/encode_session.php
@@ -9,4 +9,10 @@ $session_vars = session_encode();
 print $session_vars."<br>";
 print strlen($session_vars);
 
+$my_session_vars = session_decode($session_vars); //it return true or fulse
+var_dump($session_vars);
+echo strlen($session_vars);
+echo $_SESSION['username'];
+// var_dump($my_session_vars);
+
 ?>
