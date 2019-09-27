979bfef9df27832207cfd6e187eaded43903395e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-27 12:23:57 +0200

    try to delete cookie by set secound parameter to false or empty value

--- ch17/index.php
+++ ch17/index.php
@@ -4,5 +4,10 @@
    //send cookie by function setcookie and take name and value
    setcookie('cookieName','this is the cookie value');
    setcookie($name ,$age);
+   echo"Hello";
+   //to delete cookie
+   setcookie('cookieName',FALSE); //first way
+   setcookie($name,''); //the secound way
+
 
 ?>
