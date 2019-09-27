d493db53cf1f311ee04d99a3d1c0363825f7bdf9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-27 12:28:57 +0200

    create reset page to delete the cookies in the browser

---
+++ ch17/reset.php
@@ -0,0 +1,14 @@
+<?php
+    setcookie('font_size','',time()-600,'/');
+    setcookie('font_color','',time()-600,'/');
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <p>Your settings have been reset! Click
+            <a href="view_settings.php">here</a> to go back to the main page.
+        </p>
+    </body>
+</html>
