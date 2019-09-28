d056994443a39b6a9b8bf2009e5661b9f905e32e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-28 19:30:22 +0200

    connect to the database and server

--- ch17/session/practical_examples/exampleOfArticles/index.php
+++ ch17/session/practical_examples/exampleOfArticles/index.php
@@ -1,6 +1,13 @@
 <?php
+    define('DB_HOST','localhost');
+    define('DB_PASWD','bigbigfamily');
+    define('DB_USER','root');
+    define('DB_NAME','php-study');
     //start session
     session_start();
+
+    //connect to server and database
+    $connection = new mysqli(DB_HOST,DB_USER,DB_PASWD,DB_NAME);
 
 
 ?>
