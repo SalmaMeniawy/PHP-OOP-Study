b80bc6bca653a8652bfd5fd61a453051d0d865bc
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-09-28 19:37:47 +0200

    retrive all articles in the database

--- ch17/session/practical_examples/exampleOfArticles/index.php
+++ ch17/session/practical_examples/exampleOfArticles/index.php
@@ -9,5 +9,15 @@
     //connect to server and database
     $connection = new mysqli(DB_HOST,DB_USER,DB_PASWD,DB_NAME);
 
+    //retreve all articles from database
+    $stmt = $connection->prepare('SELECT id , title , content FROM articles WHERE id = ?');
+    $stmt->bind_param('s',$_GET['id']);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows == 1)
+    {
+        $stmt->bind_result($id , $title ,$content);
+        $stmt->fetch();
+    }
 
 ?>
