dea19060f7c5e10f9fa5116643e07d075d6f9b9a
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-02 23:14:40 +0200

    Set config params to acces Google API

--- googleOAuth/google_oauth_config.php
+++ googleOAuth/google_oauth_config.php
@@ -2,5 +2,10 @@
     session_start();
     //include Google API Libaray
     require_once('./composer/vendor/autoload.php');
+    //set the config params for google API
+    $client_id = '1044733286399-7n77coc95l8d2mt6llo07rf70ktdv6rp.apps.googleusercontent.com';
+    $client_secret = 'P1b_weyo7ODZCw6_ixgXO9wH';
+    $redirect_url = 'https://salamonty.com/google-oauth-php';
+    
 
 ?>
