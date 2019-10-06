d0f20a3a8bf6af2371b14f8bed31c0e3461bcf3c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-06 14:50:45 +0200

    try db_method

---
+++ ch27/index.php
@@ -0,0 +1,15 @@
+
+<?php
+    //define config relates to connection
+    define('DB_HOST',"localhost");
+    define('DB_USER',"root");
+    define('DB_PSWD','bigbigfamily');
+    define('DB_NAME',"corporate");
+    // $connection = new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+    $connection = new mysqli();
+    $connection->connect(DB_HOST,DB_USER,DB_PSWD);
+    $connection->select_db(DB_NAME);
+
+  
+
+?>
