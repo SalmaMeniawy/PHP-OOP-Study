b795e5330cc01b8ba8b55de4a97e8a95a0d71408
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-06 15:58:48 +0200

    check if there error code if there .. in server terminal the message of error will display

--- ch27/index.php
+++ ch27/index.php
@@ -4,13 +4,20 @@
     define('DB_HOST',"localhost");
     define('DB_USER',"root");
     define('DB_PSWD','bigbigfamily');
-    define('DB_NAME',"corporate");
+    define('DB_NAME',"corporate1");
     $connection = new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+    if ($connection->connect_errno) {
+        // printf("Unable to connect to the database:<br /> %s",
+        // $connection->connect_error);
+        // exit();
+        error_log(print_r("Unable to connect to the database:{$connection->connect_error}",
+        $connection->connect_error));
+    }
     // $connection = new mysqli();
     // $connection->connect(DB_HOST,DB_USER,DB_PSWD);
     // $connection->select_db(DB_NAME);
     // mysqli_connect_error();
-    printf("mysql Error number generated : %d ",$connection->connect_errno);
-    printf("mysql Error number generated : %d ",$connection->errno);
+    // printf("mysql Error number generated : %d ",$connection->connect_errno);
+    // printf("mysql Error number generated : %d ",$connection->errno);
 
 ?>
