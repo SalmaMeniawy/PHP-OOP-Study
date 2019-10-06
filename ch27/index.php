e1cfe6ea988e98f0aad627caaa73bb086f297934
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-06 15:16:48 +0200

    try and use proberty connect_errno in instance of mysqli

--- ch27/index.php
+++ ch27/index.php
@@ -5,11 +5,11 @@
     define('DB_USER',"root");
     define('DB_PSWD','bigbigfamily');
     define('DB_NAME',"corporate");
-    // $connection = new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
-    $connection = new mysqli();
-    $connection->connect(DB_HOST,DB_USER,DB_PSWD);
-    $connection->select_db(DB_NAME);
-
-  
+    $connection = new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+    // $connection = new mysqli();
+    // $connection->connect(DB_HOST,DB_USER,DB_PSWD);
+    // $connection->select_db(DB_NAME);
+    // mysqli_connect_error();
+    printf("mysql Error number generated : %d ",$connection->connect_errno);
 
 ?>
