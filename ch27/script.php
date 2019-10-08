3a7375d0472ddc662cbaf7e3214d4390280dbcf7
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-08 19:08:02 +0200

    try to use method free in class mysqli

---
+++ ch27/script.php
@@ -0,0 +1,19 @@
+<?php
+//define config relates to connection
+define('DB_HOST',"localhost");
+define('DB_USER',"root");
+define('DB_PSWD','bigbigfamily');
+define('DB_NAME',"corporate");
+$connection = new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+$query = "SELECT sku , name , price FROM products ORDER by name";
+$result = $connection->query($query,MYSQLI_STORE_RESULT);
+while(list($sku , $name , $price)=$result->fetch_row())
+{
+    printf("(%s) %s: \$%s <br />", $sku, $name, $price);
+
+}
+$result->free();
+
+
+
+?>
